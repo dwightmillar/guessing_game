d95d705672c4fbdd2ca3675a1d81ca1ba0518c83
Dwight Millar <millar.dwight@gmail.com>
2019-08-12 13:39:32 -0700

    added function to change text according to input value

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 var the_number;
+var the_guess;
 
-function generateRandomNumber() {
-  the_number = Math.ceil(Math.random()) * 10;
+window.onload = function generateRandomNumber() {
+  the_number = Math.ceil(Math.random() * 10);
+}
+
+
+
+function getInputSendResponse() {
+  the_guess = document.getElementById('guess_input').value;
+  console.log(the_number);
+
+  if (the_guess < the_number) {
+    document.getElementById('response_div').innerText = 'Too low!';
+  }
+  if (the_guess > the_number) {
+    document.getElementById('response_div').innerText = 'Too high!';
+  }
+  if (the_guess == the_number) {
+    console.log('click');
+    document.getElementById('response_div').innerText = 'You guessed it!';
+  }
 }
